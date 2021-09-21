7f30ed95000006bb1da3526ff27752119fe7540f
marvelmikel <49599133+marvelmikel@users.noreply.github.com>
2021-09-22 00:06:24 +0100

    second commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,33 +15,34 @@
 			<div class="row">
 				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">
 					<div class=" main-content-area">
-						<div class="wrap-login-item ">						
+						<div class="wrap-login-item ">
 							<div class="login-form form-item form-stl">
+                                
 								<form name="frm-login" method="POST" action="{{route('login')}}">
                                     @csrf
 									<fieldset class="wrap-title">
-										<h3 class="form-title">Log in to your account</h3>										
+										<h3 class="form-title">Log in to your account</h3>
 									</fieldset>
 									<fieldset class="wrap-input">
 										<label for="frm-login-uname">Email Address:</label>
-										<input type="email" id="frm-login-uname" name="email" placeholder="Type your email address" val>
+										<input type="email" id="frm-login-uname" name="email" placeholder="Type your email address": value="old('email')" required autofocus>
 									</fieldset>
 									<fieldset class="wrap-input">
 										<label for="frm-login-pass">Password:</label>
-										<input type="password" id="frm-login-pass" name="pass" placeholder="************">
+										<input type="password" id="frm-login-pass" name="password" placeholder="************" required autocomplete="current-password">
 									</fieldset>
-									
+
 									<fieldset class="wrap-input">
 										<label class="remember-field">
-											<input class="frm-input " name="rememberme" id="rememberme" value="forever" type="checkbox"><span>Remember me</span>
+											<input class="frm-input " name="remember" id="rememberme" value="forever" type="checkbox"><span>Remember me</span>
 										</label>
-										<a class="link-function left-position" href="#" title="Forgotten password?">Forgotten password?</a>
+										<a class="link-function left-position" href="{{route ('password.request') }}" title="Forgotten password?">Forgotten password?</a>
 									</fieldset>
 									<input type="submit" class="btn btn-submit" value="Login" name="submit">
 								</form>
-							</div>												
+							</div>
 						</div>
-					</div><!--end main products area-->		
+					</div><!--end main products area-->
 				</div>
 			</div><!--end row-->
 
